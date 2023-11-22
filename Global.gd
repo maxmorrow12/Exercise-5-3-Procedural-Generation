1031ec3cd0eede8d628d2a4b58e70a930afc266e
maxmorrow12 <maxamorr@iu.edu>
2023-11-22 09:30:33 -0600

    Completes Exercise

--- Global.gd
+++ Global.gd
@@ -1,5 +1,18 @@
 extends Node
 
+func_ready(): 
+	process_mode = Node.PROCESS_MODE_ALWAYS
+
 func _unhandled_input(event):
 	if event.is_action_pressed("Menu"):
+		var Menu = get_node_or_null("/root/Game/Menu")
+		if Menu == null:
+			get_tree().quit()
+		else:
+			if not Menu.visible:
+				Menu.show()
+				get_tree().paused = true 	# pause the game while the menu is visible
+			else:
+				Menu.hide()
+				get_tree().paused = false
 		get_tree().quit()
